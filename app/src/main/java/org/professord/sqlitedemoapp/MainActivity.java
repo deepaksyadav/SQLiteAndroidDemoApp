00a1afd7a071aefc40167a96156589b8061cfe97
deepaksyadav <deepaksyadav@users.noreply.github.com>
2021-06-18 15:25:44 +0530

    Database read operation demo

--- app/src/main/java/org/professord/sqlitedemoapp/MainActivity.java
+++ app/src/main/java/org/professord/sqlitedemoapp/MainActivity.java
@@ -2,7 +2,14 @@ package org.professord.sqlitedemoapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.widget.TextView;
+import android.widget.Toast;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,10 +18,36 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SqliteDemoHelper helper = new SqliteDemoHelper(this);
-        //SqliteDemoHelper helper = new SqliteDemoHelper(this);
-        SQLiteDatabase db = helper.getReadableDatabase();
 
+        try {
+            SqliteDemoHelper helper = new SqliteDemoHelper(this);
+            int drinkNo = 2;
+
+            SQLiteDatabase db = helper.getReadableDatabase();
+            Cursor cursor = db.query("DRINK",
+                    new String[]{"NAME", "DESCRIPTION"},
+                    "_id = ?",
+                    new String[]{Integer.toString(drinkNo)},
+                    null, null, null);
+            //Move to the first record in the Cursor
+            if (cursor.moveToFirst()) {
+//Get the drink details from the cursor
+                String nameText = cursor.getString(0);
+                String descriptionText = cursor.getString(1);
+                //int photoId = cursor.getInt(2);
+//Populate the drink name
+                TextView name = (TextView) findViewById(R.id.name);
+                name.setText(nameText);
+//Populate the drink description
+                TextView description = (TextView) findViewById(R.id.description);
+                description.setText(descriptionText);
+            }
+            cursor.close();
+            db.close();
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+
+        }
     }
-
 }
